eabc259771b24f92723f71848176eb748c227a42
kaditya67 <322adityakumar0004@dbit.in>
2024-12-12 19:41:39 +0530

    connected to mongo db

---
+++ vidtube/src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import {DB_NAME} from '../constants.js';
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+        console.log(`Connected to MongoDB at host: ${connectionInstance.connection.host}, database: ${connectionInstance.connection.name}`);
+
+    } catch (error) {
+        console.log("Failed to connnect to Mongo DB ", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
